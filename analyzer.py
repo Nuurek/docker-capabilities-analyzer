4da86e840efc05d0e246726686a85e28d12cc8ab
Jakub Nurski <nurskijakub@gmail.com>
2019-09-23 21:33:38 +0100

    Initial commit with analyzer plan.

---
+++ analyzer.py
@@ -0,0 +1,11 @@
+# read the input
+
+# start the container
+
+# get container's pid and declared capabilities
+
+# trace fork -> update children pids
+
+# trace kill -> update children pids
+
+# trace capable -> check if in pids and add cap to set
