c4ea332baf2a8beafb2cfaf2f59af0d64afca84d
Jakub Nurski <nurskijakub@gmail.com>
2019-09-24 13:18:41 +0100

    Added capabilities.

--- analyzer.py
+++ analyzer.py
@@ -10,6 +10,7 @@ import uuid
 import docker
 from docker.models.containers import Container
 
+from capabilites import Capabilities, DEFAULT_CAPABILITIES
 from parser import Parser
 
 args = Parser.get_arguments()
@@ -26,11 +27,19 @@ parameters = {
     'environment': args.env,
     'ports': args.publish,
     'volumes': args.volume,
+    'cap_add': args.cap_add,
+    'cap_drop': args.cap_drop
 }
 
 print('Running container')
 container: Container = client.containers.run(**parameters)
 print('Ran container')
+
+host_config = container.attrs['HostConfig']
+added_capabilities = Capabilities.from_strings(host_config['CapAdd'])
+dropped_capabilities = Capabilities.from_strings(host_config['CapDrop'])
+
+capabilities = DEFAULT_CAPABILITIES.union(added_capabilities).difference(dropped_capabilities)
 
 
 def signal_handler(_signal_number, _frame):
